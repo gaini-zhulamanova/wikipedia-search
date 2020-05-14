f35dab64256fb0022dc43c27dccb1105f1dc8db5
gaini-zhulamanova <gaini.zhulamanova@gmail.com>
2020-05-14 15:26:02 +0200

    add page-list, search-bar, service components, and styling

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  // create an instance of the EventEmitter
+  @Output() submitted = new EventEmitter<string>();
+
+  term = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFormSubmit(event: any) {
+    // prevent default browser behavior: tries to send the value at once to the backend
+    event.preventDefault();
+    // send the term to the parent component
+    this.submitted.emit(this.term);
   }
 
 }
